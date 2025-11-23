e83fd2cf2a8bbfb4efb840aff03269f2bb4d3dcb
ddimatos <dimatos@gmail.com>
2025-11-22 23:23:45 -0800

    Update script for portability and attributes
    Signed-off-by: ddimatos <dimatos@gmail.com>

--- scripts/curl-job-template.sh
+++ scripts/curl-job-template.sh
@@ -13,7 +13,7 @@ export SEP_LINE_NL="\n----------------------------------------------------------
 
 if [ -f "$ENV_SOURCE_FILE" ]; then
   echo "${SEP_LINE_NL}[INFO] Sourcing $ENV_SOURCE_FILE${SEP_LINE_NL}"
-  source "$ENV_SOURCE_FILE"
+  . "$ENV_SOURCE_FILE"
 else
   echo "${SEP_LINE_NL}[WARN] There is no environment file $ENV_SOURCE_FILE,
   please be sure to configure script variables'USER', 'HOST' and \
@@ -35,16 +35,19 @@ echo ${response}
 # ---------------------------------------------------------------------
 # Query - job 9 template
 # ---------------------------------------------------------------------
-job_id=$( jq -r  '.id' <<< "${response}" )
+#job_id=$( jq -r  '.id' <<< "${response}" )
+job_id=$( echo "${response}"|  jq -r '.id')
 sleep 2
 echo "${SEP_LINE_NL}[INFO] Get job template status using AAP REST API for job id ${job_id} ${SEP_LINE_NL}"
 result=$(curl -s -k -X GET -H "Authorization: Bearer ${AAP_AUTH_TOKEN}" -H "Content-Type: application/json" "https://${HOST_IP}/api/controller/v2/jobs/${job_id}/" |jq )
-stat=$( jq -r  '.status' <<< "${result}" )
+#stat=$( jq -r  '.status' <<< "${result}" )
+stat=$( echo "${response}"|  jq -r '.status')
 
 while [ "${stat}" = "running" ]; do
   sleep 1
   result=$(curl -s -k -X GET -H "Authorization: Bearer ${AAP_AUTH_TOKEN}" -H "Content-Type: application/json" "https://${HOST_IP}/api/controller/v2/jobs/${job_id}/" |jq )
-  stat=$( jq -r  '.status' <<< "${result}" )
+  #stat=$( jq -r  '.status' <<< "${result}" )
+  stat=$( echo "${response}"|  jq -r '.status')
   echo "[INFO] Ansible job template has not completed, status=[${stat}]"
 done
 
